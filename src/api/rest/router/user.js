4b3e55b84b644be9018a2de56afb5c89faf69c05
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-06-22 19:01:58 +0700

    Login with Email&Pass

---
+++ src/api/rest/router/user.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const router = express.Router()
+const userService = require('../../../services/user')
+const validate = require('validator')
+const UniversalError = require('../../../error/UniversalError')
+const withAuth = require('../middlewares/withAuth')
+
+router.post('/register', async (req, res, next) => {
+    try {
+        var { body } = req
+        //var errors = new UniversalError()
+        if (!body.userName) {
+            //errors.addError('empty/userName','Username was empty.');
+            await res.json('Username was empty.');
+            next(error)
+        }
+        else if (!body.password) {
+            //errors.addError('empty/userName','Password was empty.');
+            await res.json('Password was empty.');
+            return
+        }
+        else if (!body.confirmPassword) {
+            //errors.addError('empty/userName','Confirm Password was empty');
+            await res.json('Confirm Password was empty')
+            return
+        }
+        else if (body.confirmPassword !== body.password) {
+            //errors.addError('match/password','Password is not match');
+            await res.json('The password is not match.')
+            return
+        }
+        if (!body.firstName) {
+            //errors.addError('empty/userName','Firstname was empty');
+            await res.json('Firstname was empty.');
+            return
+        }
+        if (!body.lastName) {
+            //errors.addError('empty/lastName','Lastname was empty');
+            await res.json('Lastname was empty.');
+            return
+        }
+        if (!body.email) {
+            //errors.addError('empty/email','Email was empty');
+            await res.json('Email was empty');
+            return
+        }
+        else if (body.email && !validate.isEmail(body.email)) {
+            //errors.addError('invalid/email','Email was invalid');
+            await res.json('Email was invalid.')
+            return
+        }
+        if (!body.phoneNumber) {
+            //errors.addError('empty/phoneNumber','Phonenumber was empty');
+            await res.json('Phonenumber was empty')
+            return
+        }
+        // if(errors.amount > 0){
+        //     throw errors
+        // }
+
+        const user = await userService.register(body)
+        res.json({ Message: 'Done' }).status(201)
+    }
+    catch (error) {
+        next(error);
+    }
+})
+router.post('/login', async (req, res, next) => {
+    try {
+        var { body } = req
+        // var errors = new UniversalError()
+        if (!body.userName) {
+            console.log('Username was empty')
+            // errors.addError('empty/userName','Username was empty');
+        }
+        if (!body.password) {
+            console.log('Password was empty')
+            // errors.addError('empty/password','Password was empty');
+        }
+        // if (errors.amount > 0) {
+        //     throw error
+        // }
+        const user = await userService.login(body.userName, body.password)
+        await res.json(user)
+    }
+    catch (error) {
+        next(error);
+    }
+})
+router.post('/logout', withAuth, async (req, res) => {
+    const logout = await userService.revokeAccessToken(req.headers.authorization.replace('Bearer ', ''))
+    res.json(logout)
+})
+router.get('/data', withAuth, async (req, res) => {
+    try {
+        const result = await userService.getResult(accesstoken)
+        res.json(result)
+    }
+    catch (error) {
+        error
+    }
+})
+module.exports = router
---
+++ src/sevices/user.js
@@ -0,0 +1,104 @@
+const User = require('../models/userModel')
+const UniversalError = require('../error/UniversalError');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const userAuthToken = require('../models/userAuthModel')
+
+const thisService = {
+    async register(input) {
+        console.log('register called', input)
+
+        const user = new User();
+        user.userName = input.userName,
+            user.passwordHash = sha1(input.password),
+            user.firstName = input.firstName,
+            user.lastName = input.lastName,
+            user.email = input.email,
+            user.phoneNumber = input.phoneNumber
+
+
+        var isExistEmail = await User.findOne({ email: input.email })
+        if (isExistEmail) {
+            console.log('The email is already use!')
+            return
+        }
+        var isExistUsername = await User.findOne({ userName: input.userName })
+        if (isExistUsername) {
+            console.log('Username is already use!')
+            return
+        }
+        await user.save()
+
+    },
+    async login(userName, password) {
+        console.log('login called', userName)
+
+        var thisUser = await User.findOne({ userName })
+
+        if (thisUser) {
+            if (thisUser.passwordHash !== sha1(password)) {
+                console.log('Password was invalid')
+
+            }
+            const accessTokenExpiresAt = new Date()
+            const signOptionsAccessToken = {
+                ...config.session.JWT,
+                expiresIn: config.auth.expires.accessToken
+            }
+            const payloadAccessToken = {
+                userName: thisUser.userName,
+                email: thisUser.email,
+                phoneNumber: thisUser.phoneNumber
+            }
+
+
+            const expiresIn = config.auth.expires.accessToken;
+            accessTokenExpiresAt.setSeconds(accessTokenExpiresAt.getSeconds() + expiresIn)
+
+
+            const accessToken = jwt.sign(payloadAccessToken, 'secret', signOptionsAccessToken)
+
+            const UserAuth = new userAuthToken()
+            UserAuth.userName = userName
+            UserAuth.accessToken = accessToken
+            UserAuth.accessTokenExpiresAt = accessTokenExpiresAt
+            await UserAuth.save()
+
+            resData = {
+                Message: 'Auth Successful', token: accessToken
+            }
+            return resData
+        }
+        else {
+            console.log({ Message: 'Username was invalid' })
+            return
+        }
+    },
+    async revokeAccessToken(accessToken) {
+        console.log('revoke called', accessToken);
+        await userAuthToken.findOneAndDelete({ accessToken })
+        const res = {
+            message: 'logged out!',
+        }
+        return res
+    },
+    async getUser(accessToken) {
+        console.log('get data', accessToken)
+        const userTokenData = userAuthToken.findOne({ accessToken })
+        if (userTokenData) {
+            const userData = User.findOne({ userName: userTokenData.userName })
+            if (userData) {
+                const result = {
+                    firstName = userData.firstName,
+                    lastName = userData.lastName,
+                    email = userData.email,
+                    phoneNumber = userData.phoneNumber
+                }
+                return result
+            }
+        }
+    }
+}
+
+
+module.exports = thisService
