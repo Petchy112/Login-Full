ea5d629413dec508fcb2620e2d05f3ad7ad2e886
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-06-22 19:02:45 +0700

    not connect database

---
+++ src/config.js
@@ -0,0 +1,14 @@
+exports = config = {
+    port: 99,
+    database: {
+        username: '',
+        password: '',
+        host: '',
+        database,
+    },
+    JWT: {
+        issuer: '',
+        alg: '',
+    }
+
+}
---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+exports = async () => {
+    let connectionValue;
+    try {
+        connectionValue = `mongodb+srv://${config.database.username}:${config.database.password}@${config.database.host}/${config.database.database}`
+        await mongoose.connect(connectionValue, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('Database Connected.')
+    }
+    catch (err) {
+        console.log('Database could not connect.')
+    }
+
+}
