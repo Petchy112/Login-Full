a12ad8ecd0709a0bf18c7ba8922ea1a68efc091f
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-06-29 18:23:06 +0700

    add edit function

--- BACK-END/services/event.js
+++ BACK-END/services/event.js
@@ -11,8 +11,26 @@ const eventService = {
             await newEvent.save()
 
             var dataRes = {
-                message: 'Add event succesfully'
+                message: 'Add succesfully'
 
+            }
+            return dataRes
+        } catch (err) {
+            var dataRes = 'error'
+            return dataRes
+        }
+    },
+    async editEvent(topicId, topic, description) {
+        try {
+            console.log('edit event called', topicId);
+
+            await Event.findOneAndUpdate({ _id: topicId }, {
+                topic: topic,
+                description: description
+            })
+
+            var dataRes = {
+                message: 'Edit succesfully'
             }
             return dataRes
         } catch (err) {
