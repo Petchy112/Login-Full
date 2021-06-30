031e69597f65a5433b4f222388e2bb683a67eafa
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-06-30 19:13:44 +0700

    event detail

--- BACK-END/api/rest/router/event.js
+++ BACK-END/api/rest/router/event.js
@@ -17,7 +17,14 @@ router.post('/newEvent', withAuth, async (req, res) => {
     const event = await eventService.addEvent(body.topic, body.description)
     res.json(event);
 })
-router.put('/edit/:_id', async (req, res) => {
+router.get('/event/:_id', withAuth, async (req, res) => {
+    var topic = req.params._id
+    console.log(topic);
+    await Event.findById((topic), (err, result) => {
+        res.json({ result })
+    })
+})
+router.put('/event/edit/:_id', async (req, res) => {
     const event = await eventService.editEvent(req.params._id, req.body.topic, req.body.description)
     res.json(event);
 })
