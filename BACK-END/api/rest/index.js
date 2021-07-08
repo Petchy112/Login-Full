78745ad761e3ae4f1b845ef8f82a4f3fc248e2e2
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-07-08 23:57:42 +0700

    add http-error and test cookieParser

--- BACK-END/api/rest/index.js
+++ BACK-END/api/rest/index.js
@@ -3,28 +3,30 @@ const app = express();
 const cors = require('cors');
 const config = require('../../config');
 const routes = require('./router/index')
-
+const createError = require('http-errors')
+const cookieParser = require('cookie-parser')
 
 app.use(cors());
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static('public'));
 routes(app);
 
-app.use((error, req, res, next) => {
-    if (error.message === 'nothing') {
-        return
-    }
-    if (error.universal) {
-        const status = error.status
-        error.status = undefined
-        error.amount = undefined
-        error.universal = undefined
-        res(status).json({ error: [{ message: error.message, ...error }] })
-        return
-    }
-    throw error
+app.use((req, res, next) => {
+    next(createError(404, 'Not Found'));
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
 });
 
 console.log(`Starting with port ${config.port}`);
